a8616b96f54f9f9f87e1685f37bacebf1624759e
vietanh95 <dvanh271295@gmail.com>
2022-05-26 18:03:24 +0700

    add

---
+++ src/pages/product/Integrated/components/Deliver.js
@@ -0,0 +1,23 @@
+export default function Deliver () {
+  return (
+    <>
+    <div>
+      <ContainerMain>
+        <div>
+          <div>
+            <div>
+              <div><img /></div>
+              <div>
+                <div>Real-Time Order Tracking</div>
+                <div>View, track and process your orders and stay up-to-date at all times</div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </ContainerMain>
+    </div>
+    </>
+  )
+}
