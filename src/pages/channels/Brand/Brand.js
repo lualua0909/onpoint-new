14ae60ad810e2bb7105ee9e604ddbed82944e4ab
vietanh95 <dvanh271295@gmail.com>
2022-05-27 13:18:16 +0700

    add

--- src/pages/channels/Brand/Brand.js
+++ src/pages/channels/Brand/Brand.js
@@ -1,20 +1,29 @@
 import BannerHeader from "pages/product/Common/BannerHeader"
 import GetStart from "pages/product/Common/GetStart"
+import NewsBrand from "pages/channels/Brand/components/NewsBrand"
+import LogoFooter from 'pages/service/Common/LogoFooter'
 
 import banner from "images/channels/Brand/banner-1.png"
+import logo1 from "images/channels/Brand/logo-1.png"
+import logo2 from "images/channels/Brand/logo-2.png"
 
 export default function Brand() {
   return (
     <>
       <BannerHeader
-        breadcum="Home/ Products/ Online-Offline Integrated Order Management"
+        breadcum="Home/ Channels/ Brand.com"
         banner={banner}
         logo={''}
-        listTitle={["Online-Offline Integrated", "Order Management"]}
+        listTitle={["Brand.com"]}
         description={
-          "O2O Order Management System to Enable Omni-channel Retail "
+          "We offer a powerful tool to help you increase your ownership of customer data and directly engage with customers along their shopping journey "
         }
       />
+      <NewsBrand />
+      <LogoFooter title="In Compatible With " description="" list={[
+        logo1,
+        logo2,
+      ]} />
       <GetStart />
     </>
   )
---
+++ src/pages/channels/Brand/components/NewsBrand.js
@@ -0,0 +1,87 @@
+import ContainerMain from "pages/common/ContainerMain"
+
+import new1 from 'images/Channels/Brand/new-1.png'
+import new2 from 'images/Channels/Brand/new-2.png'
+import new3 from 'images/Channels/Brand/new-3.png'
+import new4 from 'images/Channels/Brand/new-4.png'
+
+import './NewsBrand.scss'
+
+export default function NewsBrand() {
+  return (
+    <>
+      <div className='dwwyzparpf'>
+        <ContainerMain>
+          <div className='jocsazxeyz'>
+            <div className='oywxbgmyxe'>A comprehensive solution with powerful yet easy-to-use feature set for business users</div>
+            <div className='wzgsabgpgm'>This is a more direct-to-consumers approach, aimed at your core target audience group. Having your
+              own brand.com is a powerful tool to enhance your online presence, manage your business image,
+              product lisitng and the seamless end-to-end user experience. We focus on user-centric designs to
+              make shopping online an effortless activity for your customers.</div>
+            <div className="lqrfsnhyvi">
+              <div className='vezulrerem'>
+                <div className='qenwmwtibd'>
+                  <img className='mjxsjbzjkq' alt="" src={new1} />
+                </div>
+                <div className='ngdwdevsvj tybnthcjgj'>
+                  <div className='fciywfguey'>Data Integration</div>
+                  <ul className='popglkzayf'>
+                    <li className='mmssgjsqni'>Collect & consolidate customer data from across multiple systems (app, web, CRM, POS, data warehouses)</li>
+                    <li className='mmssgjsqni'>Stream customer data to external systems for BI analytics </li>
+                  </ul>
+                  <div className='fciywfguey'>Data Activation</div>
+                  <div className='mmssgjsqni'>Multiple communication channels with personalized messages across the journey</div>
+                </div>
+              </div>
+              <div className='vezulrerem'>
+                <div className='ngdwdevsvj tybnthcjgj'>
+                  <div className='fciywfguey'>Single View of Customer</div>
+                  <div className='mmssgjsqni'>Multiple communication channels with personalized messages across the journey</div>
+                  <div className='fciywfguey'>Customer Segmentation</div>
+                  <ul className='popglkzayf'>
+                    <li className='mmssgjsqni'>Personalized product recommendations</li>
+                    <li className='mmssgjsqni'>Exclusive web experience with personalized banners, content, promotion, products based on historical data</li>
+                  </ul>
+                </div>
+                <div className='qenwmwtibd'>
+                  <img className='mjxsjbzjkq' alt="" src={new2} />
+                </div>
+              </div>
+              <div className='vezulrerem'>
+                <div className='qenwmwtibd'>
+                  <img className='mjxsjbzjkq' alt="" src={new3} />
+                </div>
+                <div className='ngdwdevsvj tybnthcjgj'>
+                  <div className='fciywfguey'>Journey Orchestration</div>
+                  <ul className='popglkzayf'>
+                    <li className='mmssgjsqni'>Craft automated cross-channel journeys with trigger events</li>
+                    <li className='mmssgjsqni'>Personalize the communication at every touchpoint of the journey for each segment.</li>
+                  </ul>
+                  <div className='fciywfguey'>Personalization</div>
+                  <ul className='popglkzayf'>
+                    <li className='mmssgjsqni'>Personalized product recommendations</li>
+                    <li className='mmssgjsqni'>Exclusive web experience with personalized banners, content, promotion, products based on historical data </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='vezulrerem'>
+                <div className='ngdwdevsvj tybnthcjgj'>
+                  <div className='fciywfguey'>User Path Optimization</div>
+                  <div className='mmssgjsqni'>Dynamic A/B Testing and AI engine to figure out the optimal journey & channel, pick the best content and deliver it at the perfect moment to maximize conversions.</div>
+                  <div className='fciywfguey'>Data Analytics</div>
+                  <ul className='popglkzayf'>
+                    <li className='mmssgjsqni'>Access insightful real-time reports of all ongoing campaigns.</li>
+                    <li className='mmssgjsqni'>Discover underperforming campaigns and optimize them for better conversion.</li>
+                  </ul>
+                </div>
+                <div className='qenwmwtibd'>
+                  <img className='mjxsjbzjkq' alt="" src={new4} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </ContainerMain>
+      </div>
+    </>
+  )
+}
